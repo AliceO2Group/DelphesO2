ebaf0022a837c17c38589bf9a0ac1cf4a0565b76
Roberto Preghenella <preghenella@bo.infn.it>
2020-08-20 09:15:15 +0200

    Install relevant files and pick them from there

--- examples/scripts/createO2tables.sh
+++ examples/scripts/createO2tables.sh
@@ -3,19 +3,29 @@
 NRUNS=1
 NEVENTS=1000
 
-cp ../cards/propagate.2kG.tcl .
-cp ../smearing/luts/lutCovm.* .
-rm delphes.root
+### copy relevant files in the working directory
+cp $DELPHESO2_ROOT/examples/cards/propagate.2kG.tcl .
+cp $DELPHESO2_ROOT/examples/smearing/luts/lutCovm.* .
+cp $DELPHESO2_ROOT/examples/pythia8/pythia8_ccbar.cfg .
+# cp $DELPHESO2_ROOT/examples/aod/createO2tables.C .
+
+### loop over runs
 for I in $(seq 1 $NRUNS); do
 
-    cp pythia8_inel.cfg pythia8.cfg
+    ### copy pythia8 configuration and adjust it
+    cp pythia8_ccbar.cfg pythia8.cfg
     echo "Main:numberOfEvents $NEVENTS" >> pythia8.cfg
     echo "Random:seed = $I" >> pythia8.cfg
-    
+
+    ### run Delphes and analysis
     DelphesPythia8 propagate.2kG.tcl pythia8.cfg delphes.root &&
 	root -b -q -l "createO2tables.C(\"delphes.root\", \"AODRun5.$I.root\")" &&
 	rm -rf delphes.root
+
 done
 
+### merge runs
 hadd -f AODRun5Tot.root AODRun5.*.root && rm -rf AODRun5.*.root
-source clean.sh
+
+### clean
+# rm *.tcl *.cfg *.dat *.C
