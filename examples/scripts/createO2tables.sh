a554209e7a1bdeda27b337213d2e7d7c02692a55
Gian Michele Innocenti <ginnocen@cern.ch>
2020-08-23 17:22:26 +0200

    Update creation of O2 derived tables (#2)
    * Update O2 delphes to O2 table creation
    
    * revert wrong change on the force decay file
    
    * Limit study to 2 prongs
    
    * Use consistently 0.2 T as magnetic field
    
    * Use consistently 0.2 T as magnetic field

--- examples/scripts/createO2tables.sh
+++ examples/scripts/createO2tables.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 
 NRUNS=1
-NEVENTS=1000
-
+NEVENTS=10000
+DOANALYSIS=1
 ### copy relevant files in the working directory
 cp $DELPHESO2_ROOT/examples/cards/propagate.2kG.tcl .
 cp $DELPHESO2_ROOT/examples/smearing/luts/lutCovm.* .
@@ -30,6 +30,32 @@ done
 
 ### merge runs
 hadd -f AODRun5Tot.root AODRun5.*.root && rm -rf AODRun5.*.root
+
+FILEOUTO2="AnalysisResults.root"
+AOD3NAME=AODRun5Tot.root
+
+### perform O2 analysis
+if [ $DOANALYSIS -eq 1 ]; then
+  LOGFILE="log_o2.log"
+  echo -e "\nRunning the tasks with O2... (logfile: $LOGFILE)"
+  rm -f $FILEOUTO2
+  if [ ! -f "$AOD3NAME" ]; then
+    echo "Error: File $AOD3NAME does not exist."
+    exit 1
+  fi
+  O2ARGS="--shm-segment-size 16000000000 --configuration json://$PWD/dpl-config_std.json --aod-file $AOD3NAME"
+  O2EXEC="o2-analysis-hftrackindexskimscreator $O2ARGS | o2-analysis-hfcandidatecreator2prong $O2ARGS | o2-analysis-taskdzero $O2ARGS -b"
+  TMPSCRIPT="tmpscript.sh"
+  cat << EOF > $TMPSCRIPT # Create a temporary script with the full O2 commands.
+#!/bin/bash
+$O2EXEC
+EOF
+  $ENVO2 bash $TMPSCRIPT # Run the script in the O2 environment.
+  #$ENVO2 bash $TMPSCRIPT > $LOGFILE 2>&1 # Run the script in the O2 environment.
+  #if [ ! $? -eq 0 ]; then echo "Error"; exit 1; fi # Exit if error.
+  rm -f $TMPSCRIPT
+fi
+
 
 ### clean
 rm *.tcl *.cfg *.dat *.C
