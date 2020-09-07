6f63675001c1f5211973bca6e3315376d8da1783
Gian Michele Innocenti <ginnocen@cern.ch>
2020-09-07 20:01:31 +0200

    clena

--- examples/scripts/createO2tables.sh
+++ examples/scripts/createO2tables.sh
@@ -84,7 +84,7 @@ for I in $(seq 0 $(($NRUNS - 1))); do
     #$(($I*$NEVENTS))
     DelphesPythia8 propagate.tcl pythia8.$I.cfg delphes.$I.root &> delphes.$I.log && \
 	root -b -q -l "createO2tables.C(\"delphes.$I.root\", \"AODRun5.$I.root\", 0)" &> createO2tables.$I.log && \
-	rm -rf delphes.$I.root &&
+	#	rm -rf delphes.$I.root && \
 	rm -rf .running.$I && \
 	echo " --- complete run $I" &
 
@@ -95,7 +95,3 @@ echo " --- waiting for runs to be completed "
 wait
 echo " --- all runs are processed, merging "
 ls AODRun5.*.root >> listfiles.txt
-#hadd -f AODRun5Tot.root AODRun5.*.root
-
-### clean
-rm *.tcl *.cfg *.dat *.C
