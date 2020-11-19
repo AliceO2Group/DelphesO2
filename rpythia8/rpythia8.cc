1af198d8fc970e9b6907117609257a36278fc70f
Roberto Preghenella <preghenella@bo.infn.it>
2020-11-19 07:00:02 +0100

    rpythia: now can inject Pythia8 events on top of a Pythia8 background

--- rpythia8/rpythia8.cc
+++ rpythia8/rpythia8.cc
@@ -4,13 +4,11 @@
 #include "Pythia8/Pythia.h"
 #include "Pythia8Plugins/HepMC2.h"
 
-using namespace Pythia8;
-
 int main(int argc, char **argv)
 {
   
   int nevents;
-  std::string config, output;
+  std::string config, output, inject;
   
   /** process arguments **/
   namespace po = boost::program_options;
@@ -18,9 +16,10 @@ int main(int argc, char **argv)
   try {
     desc.add_options()
       ("help", "Print help messages")
-      ("nevents,n" , po::value<int>(&nevents)->default_value(10), "Number of events")
-      ("config,c"  , po::value<std::string>(&config), "Configuration file")
-      ("output,o"  , po::value<std::string>(&output)->default_value("pythia8.hepmc"), "Output HepMC file")
+      ("nevents,n"     , po::value<int>(&nevents)->default_value(10), "Number of events")
+      ("config,c"      , po::value<std::string>(&config), "Configuration file")
+      ("output,o"      , po::value<std::string>(&output)->default_value("pythia8.hepmc"), "Output HepMC file")
+      ("inject,i"      , po::value<std::string>(&inject), "Injected event configuration file")
       ;
     
     po::variables_map vm;
@@ -38,10 +37,6 @@ int main(int argc, char **argv)
     return 1;
   }
 
-  // Interface for conversion from Pythia8::Event to HepMC event.
-  HepMC::Pythia8ToHepMC ToHepMC;
-  HepMC::IO_GenEvent ascii_io(output, std::ios::out); 
-  
   // pythia, config and init
   Pythia8::Pythia pythia;
   Pythia8::Rndm   rndm;
@@ -52,24 +47,50 @@ int main(int argc, char **argv)
   pythia.init();
   rndm.init();
 
+  // Interface for conversion from Pythia8::Event to HepMC event.
+  HepMC::Pythia8ToHepMC ToHepMC;
+  HepMC::IO_GenEvent output_io(output, std::ios::out);
+
+  // injection interface
+  Pythia8::Pythia *pythia_inj = nullptr;
+  if (!inject.empty()) {
+    std::cout << "Injection: configure from " << inject << std::endl;
+    pythia_inj = new Pythia8::Pythia;
+    if (!pythia_inj->readFile(inject)) {
+      std::cout << "Error: could not read config file \"" << config << "\"" << std::endl;
+      return 1;
+    }
+    pythia_inj->init();
+  }
+  
   // event loop
   for (int iev = 0; iev < nevents; ++iev) {
-
+    
     // generate event
     pythia.next();
-
-    // photon conversions 
+    auto offset = pythia.event.size();
     
+    // injection
+    if (pythia_inj) {
+      pythia_inj->next();
+      pythia.event += pythia_inj->event;
+    }
+          
     // convert to HepMC
     HepMC::GenEvent *hepmcevt = new HepMC::GenEvent();
     ToHepMC.fill_next_event(pythia, hepmcevt);
-    ascii_io << hepmcevt;
+
+    output_io << hepmcevt;
     delete hepmcevt;
-    
+
   } // end of event loop
   
   // print statistics
   pythia.stat();
+  if (pythia_inj) {
+    pythia_inj->stat();
+    delete pythia_inj;
+  }
   
   return 0;
 }
