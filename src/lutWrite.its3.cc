23c424b7e260ac463f9e7fbc83969f50fc356398
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2021-02-10 11:00:02 +0100

    Add carbon foam to the ITS3

--- src/lutWrite.its3.cc
+++ src/lutWrite.its3.cc
@@ -13,19 +13,24 @@ void fatInit_its3(float field = 0.5, float rmin = 100.) {
   fat.AddLayer((char *)"vertex", 0, 0); // dummy vertex for matrix calculation
 
   // new ideal Pixel properties?
-  Double_t x0 = 0.0050;
-  Double_t resRPhi = 0.0006;
-  Double_t resZ = 0.0006;
-  Double_t eff = 0.98;
-  Double_t xrhoOB = 1.1646e-01; // 500 mum Si
+  Double_t x0IB = 0.0005;       // X/X0 of the inner barrel (first three layers)
+  Double_t x0OB = 0.0035;       // X/X0 of the outer barrel
+  Double_t resRPhi = 0.0006;    // Resolution in Rphi
+  Double_t resZ = 0.0006;       // Resolution in Z
+  Double_t eff = 0.98;          // Efficiency
+  Double_t xrhoIB = 1.1646e-02; // Surface density for 50 mum thick Si
+  Double_t xrhoOB = 1.1646e-01; // Surface density for 500 mum thick Si
 
-  fat.AddLayer((char *)"ddd1", 1.8, x0, xrhoOB, resRPhi, resZ, eff);
-  fat.AddLayer((char *)"ddd2", 2.4, x0, xrhoOB, resRPhi, resZ, eff);
-  fat.AddLayer((char *)"ddd3", 3.0, x0, xrhoOB, resRPhi, resZ, eff);
-  fat.AddLayer((char *)"ddd4", 19.4, x0, xrhoOB, resRPhi, resZ, eff);
-  fat.AddLayer((char *)"ddd5", 24.7, x0, xrhoOB, resRPhi, resZ, eff);
-  fat.AddLayer((char *)"ddd6", 35.3, x0, xrhoOB, resRPhi, resZ, eff);
-  fat.AddLayer((char *)"ddd7", 40.5, x0, xrhoOB, resRPhi, resZ, eff);
+  fat.AddLayer((char *)"ddd1", 1.8, x0IB, xrhoIB, resRPhi, resZ, eff);
+  fat.AddLayer((char *)"foam1", 1.8 + 9.370 * x0IB, 0.0008); // Foam spacer
+  fat.AddLayer((char *)"ddd2", 2.4, x0IB, xrhoIB, resRPhi, resZ, eff);
+  fat.AddLayer((char *)"foam2", 2.4 + 9.370 * x0IB, 0.0008); // Foam spacer
+  fat.AddLayer((char *)"ddd3", 3.0, x0IB, xrhoIB, resRPhi, resZ, eff);
+  fat.AddLayer((char *)"foam3", 3.0 + 9.370 * x0IB, 0.0008); // Foam spacer
+  fat.AddLayer((char *)"ddd4", 19.4, x0OB, xrhoOB, resRPhi, resZ, eff);
+  fat.AddLayer((char *)"ddd5", 24.7, x0OB, xrhoOB, resRPhi, resZ, eff);
+  fat.AddLayer((char *)"ddd6", 35.3, x0OB, xrhoOB, resRPhi, resZ, eff);
+  fat.AddLayer((char *)"ddd7", 40.5, x0OB, xrhoOB, resRPhi, resZ, eff);
 
   fat.AddTPC(0.1, 0.1); // TPC
   fat.SetAtLeastHits(4);
