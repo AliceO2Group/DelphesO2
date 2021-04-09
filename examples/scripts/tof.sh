9b73f2de6e1c6fe14e2b8d23fc1ed7208ebee9ec
Roberto Preghenella <preghenella@bo.infn.it>
2021-04-09 12:04:52 +0200

    Initial work to bring TOF mismatch to public

--- examples/scripts/tof.sh
+++ examples/scripts/tof.sh
@@ -1,18 +1,19 @@
 #! /usr/bin/env bash
 
-NJOBS=5              # number of max parallel runs
-NRUNS=10             # number of runs
-NEVENTS=10000        # number of events in a run
+NJOBS=5                # number of max parallel runs
+NRUNS=10               # number of runs
+NEVENTS=10000            # number of events in a run
 
-LUTTAG="werner"      # LUT tag name
-BFIELD=5.            # magnetic field     [kG]
-SIGMAT=0.020         # time resolution    [ns]
-SIGMA0=0.200         # vertex time spread [ns]
-TAILLX=1.0           # tail on left       [q]
-TAILRX=1.3           # tail on right      [q]
-TOFRAD=100.          # TOF radius         [cm]
-TOFLEN=200.          # TOF half length    [cm]
-TOFETA=1.443         # TOF max pseudorapidity
+LUTTAG="werner"        # LUT tag name
+PY8CFG="pythia8_ccbar" # pythia8 configuration
+BFIELD=5.              # magnetic field     [kG]
+SIGMAT=0.020           # time resolution    [ns]
+SIGMA0=0.200           # vertex time spread [ns]
+TAILLX=1.0             # tail on left       [q]
+TAILRX=1.0             # tail on right      [q]
+TOFRAD=100.            # TOF radius         [cm]
+TOFLEN=200.            # TOF half length    [cm]
+TOFETA=1.443           # TOF max pseudorapidity
 
 ### calculate max eta from geometry
 TOFETA=`awk -v a=$TOFRAD -v b=$TOFLEN 'BEGIN {th=atan2(a,b)*0.5; sth=sin(th); cth=cos(th); print -log(sth/cth)}'`
@@ -20,8 +21,15 @@ echo "maxEta = $TOFETA"
 
 ### copy relevant files in the working directory
 cp $DELPHESO2_ROOT/examples/cards/propagate.2kG.tails.tcl propagate.tcl
-cp $DELPHESO2_ROOT/examples/pythia8/pythia8_inel.cfg .
+cp $DELPHESO2_ROOT/examples/pythia8/$PY8CFG.cfg pythia8.cfg
 cp $DELPHESO2_ROOT/examples/smearing/tof.C .
+
+### adjust pythia8 configuration
+echo "" >> pythia8.cfg
+echo "### run time configuration" >> pythia8.cfg
+echo "Main:numberOfEvents $NEVENTS" >> pythia8.cfg
+echo "Beams:allowVertexSpread on " >> pythia8.cfg
+echo "Beams:sigmaTime 60." >> pythia8.cfg
 
 ### set magnetic field
 sed -i -e "s/set barrel_Bz .*$/set barrel_Bz ${BFIELD}e\-1/" propagate.tcl
@@ -39,6 +47,9 @@ sed -i -e "s/set barrel_TailRight .*$/set barrel_TailRight ${TAILRX}/" propagate
 sed -i -e "s/set barrel_TailLeft  .*$/set barrel_TailLeft ${TAILLX}/" propagate.tcl
 sed -i -e "s/double tof_sigmat = .*$/double tof_sigmat = ${SIGMAT}\;/" tof.C
 sed -i -e "s/double tof_sigma0 = .*$/double tof_sigma0 = ${SIGMA0}\;/" tof.C
+### set TOF mismatch information
+sed -i -e "s/double tof_mismatch.*$/double tof_mismatch = 0.01;/" tof.C
+#sed -i -e "s/std::string tof_mismatch_fname.*$/std::string tof_mismatch_fname = \"tof_mismatch_template.root\";/" tof.C
 
 ### create LUTs
 BFIELDT=`awk -v a=$BFIELD 'BEGIN {print a*0.1}'`
@@ -58,12 +69,10 @@ for I in $(seq 1 $NRUNS); do
     echo " --- starting run $I"
     touch .running.$I
     
-    ### copy pythia8 configuration and adjust it
-    cp pythia8_inel.cfg pythia8.$I.cfg
-    echo "Main:numberOfEvents $NEVENTS" >> pythia8.$I.cfg
+    ### copy pythia8 configuration and set random seed
+    cp pythia8.cfg pythia8.$I.cfg
+    echo "Random:setSeed on" >> pythia8.$I.cfg
     echo "Random:seed = $I" >> pythia8.$I.cfg
-    echo "Beams:allowVertexSpread on " >> pythia8.$I.cfg
-    echo "Beams:sigmaTime 60." >> pythia8.$I.cfg
 
     ### run Delphes and analysis    
     DelphesPythia8 propagate.tcl pythia8.$I.cfg delphes.$I.root  &> delphes.$I.log &&
