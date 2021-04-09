592c68c84c48d1af3adbae180d9254a40270cf18
Roberto Preghenella <preghenella@bo.infn.it>
2021-04-09 12:40:21 +0200

    Improvements to RICH examples

--- examples/scripts/rich.sh
+++ examples/scripts/rich.sh
@@ -1,13 +1,15 @@
 #! /usr/bin/env bash
 
-NJOBS=5         # number of max parallel runs
-NRUNS=5         # number of runs
-NEVENTS=10000   # number of events in a run
+NJOBS=5                # number of max parallel runs
+NRUNS=5                # number of runs
+NEVENTS=10000          # number of events in a run
 
-BFIELD=5.       # magnetic field   [kG]
-RICHRAD=100.    # RICH radius      [cm]
-RICHLEN=200.    # RICH half length [cm]
-RICHETA=1.443   # RICH max pseudorapidity
+LUTTAG="werner"        # LUT tag name
+PY8CFG="pythia8_ccbar" # pythia8 configuration
+BFIELD=5.              # magnetic field   [kG]
+RICHRAD=100.           # RICH radius      [cm]
+RICHLEN=200.           # RICH half length [cm]
+RICHETA=1.443          # RICH max pseudorapidity
 
 ### calculate max eta from geometry
 RICHETA=`awk -v a=$RICHRAD -v b=$RICHLEN 'BEGIN {th=atan2(a,b)*0.5; sth=sin(th); cth=cos(th); print -log(sth/cth)}'`
@@ -15,13 +17,15 @@ echo "maxEta = $RICHETA"
 
 ### copy relevant files in the working directory
 cp $DELPHESO2_ROOT/examples/cards/propagate.2kG.tcl propagate.tcl
+cp $DELPHESO2_ROOT/examples/pythia8/$PY8CFG.cfg pythia8.cfg
 cp $DELPHESO2_ROOT/examples/smearing/rich.C .
-cp $DELPHESO2_ROOT/examples/pythia8/pythia8_inel.cfg pythia8.cfg
 
 ### adjust pythia8 configuration
+echo "" >> pythia8.cfg
+echo "### run time configuration" >> pythia8.cfg
 echo "Main:numberOfEvents $NEVENTS" >> pythia8.cfg
-#echo "Beams:allowVertexSpread on " >> pythia8.cfg
-#echo "Beams:sigmaTime 60." >> pythia8.cfg
+echo "Beams:allowVertexSpread off " >> pythia8.cfg
+echo "Beams:sigmaTime 60." >> pythia8.cfg
 
 ### set magnetic field
 sed -i -e "s/set barrel_Bz .*$/set barrel_Bz ${BFIELD}e\-1/" propagate.tcl
@@ -36,7 +40,7 @@ sed -i -e "s/set barrel_Acceptance .*$/set barrel_Acceptance \{ 0.0 + 1.0 * fabs
 
 ### create LUTs
 BFIELDT=`awk -v a=$BFIELD 'BEGIN {print a*0.1}'`
-$DELPHESO2_ROOT/examples/scripts/create_luts.sh werner $BFIELDT $TOFRAD
+$DELPHESO2_ROOT/examples/scripts/create_luts.sh $LUTTAG $BFIELDT $TOFRAD
 
 ### loop over runs
 rm -f .running.* delphes.*.root
@@ -54,8 +58,9 @@ for I in $(seq 1 $NRUNS); do
     
     ### copy pythia8 configuration and adjust it
     cp pythia8.cfg pythia8.$I.cfg
+    echo "Random:setSeed on" >> pythia8.$I.cfg
     echo "Random:seed = $I" >> pythia8.$I.cfg
-
+    
     ### run Delphes and analysis    
     DelphesPythia8 propagate.tcl pythia8.$I.cfg delphes.$I.root  &> delphes.$I.log &&
 	root -b -q -l "rich.C(\"delphes.$I.root\", \"rich.$I.root\")" &> rich.$I.log &&
